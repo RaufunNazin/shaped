f566c4114523da34b76f14c5f94f32b0b599db32
Kibria <kibkib62@gmail.com>
2024-01-18 17:07:20 +0600

    custom color

--- app/scatter/(components)/ScatterChart.tsx
+++ app/scatter/(components)/ScatterChart.tsx
@@ -1,8 +1,9 @@
 "use client";
 import React, {
+  Dispatch,
+  SetStateAction,
   useCallback,
   useEffect,
-  useLayoutEffect,
   useMemo,
   useRef,
   useState,
@@ -10,15 +11,13 @@ import React, {
 import moment, { Moment } from "moment";
 import { scaleLinear } from "@visx/scale";
 import useSvgPanning from "@/hooks/use-svg-panning";
+import { FaUserCircle } from "react-icons/fa";
+import { X } from "lucide-react";
 
 interface EmbeddingChartProps {
   data: { name: string | number; data: [number, number][] }[];
   dataType: string;
   colors: string[];
-}
-
-interface ColorMapping {
-  [name: string]: string;
 }
 
 const ScatterChart = ({ data, dataType, colors }: EmbeddingChartProps) => {
@@ -31,12 +30,56 @@ const ScatterChart = ({ data, dataType, colors }: EmbeddingChartProps) => {
   const [filteredData, setFilteredData] = useState<any>([]);
   const [selectedCategory, setSelectedCategory] = useState(["", ""]);
   const [mouseOverSvg, setMouseOverSvg] = useState(true);
+  const [showColorPalette, setShowColorPalette] = useState(false);
   const [widthRange, setWidthRange] = useState([padding, width - padding]);
   const [heightRange, setHeightRange] = useState([padding, height - padding]);
   const [xPercent, setXPercent] = useState(0.5);
   const [yPercent, setYPercent] = useState(0.5);
-  const colorMapping: ColorMapping = {};
+  const [categoryColors, setCategoryColors]: any = useState({});
+  const [categoryProperty, setCategoryProperty]: [
+    [string, number, number],
+    Dispatch<SetStateAction<[string, number, number]>>
+  ] = useState(["", 0, 0]);
   const svgRef = useRef<any>(null);
+
+  const paletteColors = [
+    "#64748B",
+    "#F43F5E",
+    "#EF4444",
+    "#F97316",
+    "#F59E0B",
+    "#84CC16",
+    "#22C55E",
+    "#E2E8F0",
+    "#FECDD3",
+    "#FECACA",
+    "#FED7AA",
+    "#FDE68A",
+    "#D9F99D",
+    "#BBF7D0",
+    "#14B8A6",
+    "#06B6D4",
+    "#3B82F6",
+    "#6366F1",
+    "#A855F7",
+    "#A855F7",
+    "#EC4899",
+    "#99F6E4",
+    "#A5F3FC",
+    "#BFDBFE",
+    "#C7D2FE",
+    "#E9D5FF",
+    "#F5D0FE",
+    "#F9A8D4",
+  ];
+
+  useEffect(() => {
+    const currentColors: Record<string, string> = {};
+    for (let i = 0; i < data.length; i++) {
+      currentColors[data[i].name] = colors[i % colors.length];
+    }
+    setCategoryColors(currentColors);
+  }, [data]);
 
   const handlePointHover = (point: any, value: any) => {
     setHoveredPoint({ point: point, value: value });
@@ -69,22 +112,6 @@ const ScatterChart = ({ data, dataType, colors }: EmbeddingChartProps) => {
 
     const redChannel = Math.round(255 - normalizedValue * 255);
     return `rgba(${redChannel}, 0, 0, 0.8)`;
-  };
-
-  const getColorByCategory = (name: string | number): string => {
-    if (!(name in colorMapping)) {
-      colorMapping[name] =
-        colors[Object.keys(colorMapping).length % colors.length];
-    }
-    return colorMapping[name];
-  };
-
-  const categoriesColor = (name: string | number): string => {
-    if (!(name in colorMapping)) {
-      colorMapping[name] =
-        colors[Object.keys(colorMapping).length % colors.length];
-    }
-    return colorMapping[name];
   };
 
   const [horizontalLines, verticalLines] = useMemo(() => {
@@ -272,6 +299,7 @@ const ScatterChart = ({ data, dataType, colors }: EmbeddingChartProps) => {
   const preventScroll = useCallback((e: any) => {
     e.preventDefault();
   }, []);
+
   useEffect(() => {
     if (mouseOverSvg) {
       window.addEventListener("wheel", preventScroll, {
@@ -285,50 +313,53 @@ const ScatterChart = ({ data, dataType, colors }: EmbeddingChartProps) => {
   useSvgPanning({ svg: svgRef.current, width: width, height: height });
 
   return (
-    <div>
-      <div className="flex flex-row gap-x-4">
-        <div className="relative">
-          <svg width={width} height={height} fill="none">
-            {horizontalLines.map((point, i) => {
-              return (
-                <line
-                  key={i}
-                  {...point}
-                  strokeWidth={1}
-                  stroke="#000000"
-                  strokeOpacity={0.1}
-                />
-              );
-            })}
-            {verticalLines.map((point, i) => {
-              return (
-                <line
-                  key={i}
-                  {...point}
-                  strokeWidth={1}
-                  stroke="#000000"
-                  strokeOpacity={0.1}
-                />
-              );
-            })}
-          </svg>
-          <svg
-            width={width}
-            height={height}
-            style={{
-              touchAction: "none",
-              border: "1px solid lightgray",
-              position: "absolute",
-              top: 0,
-              left: 0,
-            }}
-            fill="none"
-            ref={svgRef}
-          >
-            <rect width={width} height={height} rx={14} fill="none" />
+    <div className="flex flex-row gap-x-4">
+      <div className="relative">
+        <svg width={width} height={height} fill="none">
+          {horizontalLines.map((point, i) => {
+            return (
+              <line
+                key={i}
+                {...point}
+                strokeWidth={1}
+                stroke="#000000"
+                strokeOpacity={0.1}
+              />
+            );
+          })}
+          {verticalLines.map((point, i) => {
+            return (
+              <line
+                key={i}
+                {...point}
+                strokeWidth={1}
+                stroke="#000000"
+                strokeOpacity={0.1}
+              />
+            );
+          })}
+        </svg>
+        <svg
+          width={width}
+          height={height}
+          style={{
+            touchAction: "none",
+            border: "1px solid lightgray",
+            position: "absolute",
+            top: 0,
+            left: 0,
+          }}
+          fill="none"
+          ref={svgRef}
+        >
+          <rect width={width} height={height} rx={14} fill="none" />
 
-            <g>
-              {filteredData.map((point: any, i: number) => (
+          <g>
+            {filteredData.map(
+              (
+                point: { name: string; data: [number, number][] },
+                i: number
+              ) => (
                 <g key={i}>
                   {point.data.map(
                     (dataPoint: [number, number], dataIndex: number) => {
@@ -343,7 +374,7 @@ const ScatterChart = ({ data, dataType, colors }: EmbeddingChartProps) => {
                               ? selectedCategory[1]
                               : ["Numerical", "Timestamp"].includes(dataType)
                               ? getColor(point.name)
-                              : getColorByCategory(point.name)
+                              : categoryColors[point.name]
                           }
                           fillOpacity={0.6}
                           strokeWidth={1}
@@ -352,7 +383,7 @@ const ScatterChart = ({ data, dataType, colors }: EmbeddingChartProps) => {
                               ? selectedCategory[1]
                               : ["Numerical", "Timestamp"].includes(dataType)
                               ? getColor(point.name)
-                              : getColorByCategory(point.name)
+                              : categoryColors[point.name]
                           }
                           onMouseEnter={() => {
                             handlePointHover(point.data[dataIndex], point.name);
@@ -363,117 +394,175 @@ const ScatterChart = ({ data, dataType, colors }: EmbeddingChartProps) => {
                     }
                   )}
                 </g>
-              ))}
-            </g>
-            {hoveredPoint && (
-              <foreignObject
-                x={
-                  xScale(hoveredPoint.point[0]) + 350 > width - padding
-                    ? Math.max(0, width - 350)
-                    : xScale(hoveredPoint.point[0]) + 10
-                }
-                y={
-                  yScale(hoveredPoint.point[1]) - 70 < 0
-                    ? 10
-                    : yScale(hoveredPoint.point[1]) - 70
-                }
-                width="340"
-                height="60"
-              >
-                <div
-                  style={{
-                    width: "100%",
-                    height: "100%",
-                    padding: "10px",
-                    background: "white",
-                    border: "1px solid lightgray",
-                    borderRadius: "8px",
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    alignItems: "center",
-                  }}
-                >
-                  <div>
-                    Attribute value:{" "}
-                    <span style={{ fontWeight: "bold" }}>
-                      {hoveredPoint.value}
-                    </span>
-                  </div>
-                  <div>
-                    Embedding:{" "}
-                    <span style={{ fontWeight: "bold" }}>
-                      {hoveredPoint.point[0]}, {hoveredPoint.point[1]}
-                    </span>
-                  </div>
-                </div>
-              </foreignObject>
+              )
             )}
-          </svg>
-          <div className="absolute right-3 top-3 flex flex-col items-center">
-            <div className="flex flex-col items-center rounded-md border bg-white border-gray-200 shadow-md">
-              <button
-                type="button"
-                className="px-2 py-1"
-                onClick={handleZoomIn}
+          </g>
+          {hoveredPoint && (
+            <foreignObject
+              x={
+                xScale(hoveredPoint.point[0]) + 350 > width - padding
+                  ? Math.max(0, width - 350)
+                  : xScale(hoveredPoint.point[0]) + 10
+              }
+              y={
+                yScale(hoveredPoint.point[1]) - 70 < 0
+                  ? 10
+                  : yScale(hoveredPoint.point[1]) - 70
+              }
+              width="340"
+              height="60"
+            >
+              <div
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  padding: "10px",
+                  background: "white",
+                  border: "1px solid lightgray",
+                  borderRadius: "8px",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
               >
-                +
-              </button>
-              <hr className="w-full bg-gray-200" />
-              <button
-                type="button"
-                className="px-2 py-1"
-                onClick={handleZoomOut}
-              >
-                -
-              </button>
-              <hr className="w-full bg-gray-200" />
-              <button
-                type="button"
-                className="px-2 py-1"
-                onClick={handleZoomReset}
-              >
-                Reset
-              </button>
-            </div>
+                <div>
+                  Attribute value:{" "}
+                  <span style={{ fontWeight: "bold" }}>
+                    {hoveredPoint.value}
+                  </span>
+                </div>
+                <div>
+                  Embedding:{" "}
+                  <span style={{ fontWeight: "bold" }}>
+                    {hoveredPoint.point[0]}, {hoveredPoint.point[1]}
+                  </span>
+                </div>
+              </div>
+            </foreignObject>
+          )}
+        </svg>
+        <div className="absolute right-3 top-3 flex flex-col items-center">
+          <div className="flex flex-col items-center rounded-md border bg-white border-gray-200 shadow-md">
+            <button type="button" className="px-2 py-1" onClick={handleZoomIn}>
+              +
+            </button>
+            <hr className="w-full bg-gray-200" />
+            <button type="button" className="px-2 py-1" onClick={handleZoomOut}>
+              -
+            </button>
+            <hr className="w-full bg-gray-200" />
+            <button
+              type="button"
+              className="px-2 py-1"
+              onClick={handleZoomReset}
+            >
+              Reset
+            </button>
           </div>
         </div>
-        <div className="flex flex-col gap-y-2">
-          {!["Numerical", "Timestamp"].includes(dataType) &&
-            data.length <= 20 && (
-              <div className="text-left text-[15px] font-bold">Legend</div>
-            )}
-          {!["Numerical", "Timestamp"].includes(dataType) &&
-            data.length <= 20 &&
-            data.map((category: any) => (
-              <div key={category.name} className="text-left">
-                <button
-                  onClick={() => {
-                    if (selectedCategory[0] !== "")
-                      setSelectedCategory(["", ""]);
-                    else
-                      setSelectedCategory([
-                        category.name,
-                        categoriesColor(category.name),
-                      ]);
-                  }}
-                >
-                  <div className="flex items-center">
-                    <div
-                      className="mr-2 h-3 w-3 rounded-full"
-                      style={{
-                        backgroundColor: categoriesColor(category.name),
-                        opacity: 0.8,
-                      }}
-                    ></div>
-                    <div className="text-sm">{category.name}</div>
-                  </div>
-                </button>
-              </div>
-            ))}
-        </div>
       </div>
-      <div className="min-h-screen bg-cyan-200"></div>
+      <div className="flex flex-col gap-y-2">
+        {!["Numerical", "Timestamp"].includes(dataType) &&
+          data.length <= 20 && (
+            <div className="text-left text-[15px] font-bold">Legend</div>
+          )}
+        {!["Numerical", "Timestamp"].includes(dataType) &&
+          data.length <= 20 && (
+            <div>
+              <div className="flex items-center mb-1.5">
+                <FaUserCircle className="h-3 w-3 mr-2 text-slate-700" />
+                <div className="text-sm">user</div>
+              </div>
+              <div className="flex items-center mb-1.5">
+                <div
+                  className="mr-2 h-3 w-3 rounded-full"
+                  style={{
+                    backgroundColor: "white",
+                    border: "3px solid #334155",
+                  }}
+                ></div>
+                <div className="text-sm">items interacted with</div>
+              </div>
+              {data.map((category: any) => (
+                <div key={category.name} className="text-left">
+                  <div>
+                    <div className="flex items-center mb-1.5">
+                      <button
+                        className="mr-2 h-3 w-3 rounded-full"
+                        style={{
+                          backgroundColor: categoryColors[category.name],
+                          opacity: 0.8,
+                        }}
+                        onClick={(e) => {
+                          setShowColorPalette(true);
+                          setCategoryProperty([
+                            category.name,
+                            e.clientX,
+                            e.clientY,
+                          ]);
+                        }}
+                      ></button>
+                      <button
+                        className="text-sm"
+                        onClick={() => {
+                          if (selectedCategory[0] !== "")
+                            setSelectedCategory(["", ""]);
+                          else
+                            setSelectedCategory([
+                              category.name,
+                              categoryColors[category.name],
+                            ]);
+                        }}
+                      >
+                        {category.name}
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
+      </div>
+      {showColorPalette && (
+        <div
+          className=" bg-white shadow-lg rounded-md grid grid-cols-7 gap-1 p-4"
+          style={{
+            position: "fixed",
+            top: categoryProperty[2] + 20,
+            left: categoryProperty[1] + 20,
+          }}
+        >
+          <button className="absolute top-0 right-0">
+            <X
+              className="text-gray-300 h-5 w-5"
+              onClick={() => {
+                setShowColorPalette(false);
+                setCategoryProperty(["", 0, 0]);
+              }}
+            />
+          </button>
+          {paletteColors.map((color, i) => {
+            return (
+              <button
+                key={i}
+                className="h-3 w-3 rounded-full"
+                style={{
+                  backgroundColor: color,
+                  opacity: 1,
+                  height: 24,
+                  width: 24,
+                }}
+                onClick={() => {
+                  categoryColors[categoryProperty[0]] = color;
+                  setShowColorPalette(false);
+                  setCategoryProperty(["", 0, 0]);
+                }}
+              ></button>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
